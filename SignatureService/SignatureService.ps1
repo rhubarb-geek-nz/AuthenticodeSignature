7719f670fc84b84a19d763a3829cc70c05bb2cec
rhubarb-geek-nz <rhubarb-geek-nz@users.sourceforge.net>
2024-06-21 23:23:24 +0100

    Add verify to service

--- SignatureService/SignatureService.ps1
+++ SignatureService/SignatureService.ps1
@@ -30,25 +30,21 @@ if ( -not ( $Authorization -ccontains $request.Headers.Authorization) )
 [string]$guid = New-Guid
 
 $command = $request.Query['command'][0]
-$sha1 = $request.Query['sha1'][0]
-$fileDigest = $request.Query['fd'][0]
-$timeServer = $request.Query['t'][0]
 
-if ( $command -ne 'sign')
+if (($command -ne 'sign') -and ($command -ne 'verify'))
 {
     throw ( 'invalid command {0}' -f $command )
 }
 
 $container = Join-Path -Path $WebRootPath -ChildPath $guid
 
-$null = New-Item -Path $container -ItemType Directory
+$Null = New-Item -Path $container -ItemType Directory
 
 try
 {
     $pipelineInput.Files | ForEach-Object {
-        [Microsoft.Extensions.Logging.LoggerExtensions]::LogInformation($Logger,('Sign "{0}" with "{1}"' -f $_.FileName, $sha1),$null)
-
-        $fileName = Join-Path -Path $container -ChildPath (Split-Path -Path $_.FileName -Leaf )
+        $baseName = $_.FileName
+        $fileName = Join-Path -Path $container -ChildPath (Split-Path -Path $baseName -Leaf )
 
         $stream = [System.IO.File]::Open($fileName, [System.IO.FileMode]::Create )
 
@@ -63,26 +59,66 @@ try
                 $stream.Dispose()
             }
 
-            $codeSignCertificate = Get-ChildItem -path Cert:\ -Recurse -CodeSigningCert | Where-Object { $_.Thumbprint -eq $sha1 }
-
-            if (-not $codeSignCertificate)
+            switch ($command)
             {
-                throw ( 'no certificate for {0}' -f $sha1 )
+                'sign' {
+                    $sha1 = $request.Query['sha1'][0]
+                    $fileDigest = $request.Query['fd'][0]
+                    $timeServer = $request.Query['t'][0]
+
+                    [Microsoft.Extensions.Logging.LoggerExtensions]::LogInformation($Logger,('Sign "{0}" with "{1}"' -f $baseName, $sha1), $Null)
+
+                    $codeSignCertificate = Get-ChildItem -path Cert:\ -Recurse -CodeSigningCert | Where-Object { $_.Thumbprint -eq $sha1 }
+
+                    if (-not $codeSignCertificate)
+                    {
+                        throw ( 'no certificate for {0}' -f $sha1 )
+                    }
+
+                    $result = Set-AuthenticodeSignature -FilePath $fileName -HashAlgorithm $fileDigest -Certificate $codeSignCertificate -TimestampServer $timeServer
+
+                    [Microsoft.Extensions.Logging.LoggerExtensions]::LogInformation($Logger,('Set-AuthenticodeSignature {0} {1} {2} {3}' -f $result.SignerCertificate.Thumbprint, $result.Status, $result.StatusMessage, $result.Path), $Null)
+
+                    $response.ContentType = [Net.Mime.MediaTypeNames+Application]::Octet
+
+                    $contentDisposition = New-Object -TypeName System.Net.Mime.ContentDisposition -ArgumentList 'attachment'
+
+                    $contentDisposition.FileName = $baseName
+
+                    $response.Headers.ContentDisposition = [string]$contentDisposition
+
+                    Get-Content -LiteralPath $fileName -AsByteStream -ReadCount 0
+                }
+
+                'verify' {
+                    [Microsoft.Extensions.Logging.LoggerExtensions]::LogInformation($Logger,('Verify "{0}"' -f $baseName), $Null)
+
+                    $result = Get-AuthenticodeSignature -FilePath $fileName
+
+                    $TimeStamperCertificate = $result.TimeStamperCertificate
+                    $SignerCertificate = $result.SignerCertificate
+
+                    $hash = @{
+                        IsOSBinary = $result.IsOSBinary
+                        Path = $baseName
+                        SignatureType = $result.SignatureType.ToString()
+                        Status = $result.Status.ToString()
+                        StatusMessage = $result.StatusMessage
+                    }
+
+                    if ($TimeStamperCertificate)
+                    {
+                        $hash.TimeStamperCertificate = New-Object -TypeName System.String -ArgumentList (,[System.Security.Cryptography.PemEncoding]::Write('CERTIFICATE',$TimeStamperCertificate.RawData))
+                    }
+
+                    if ($SignerCertificate)
+                    {
+                        $hash.SignerCertificate = New-Object -TypeName System.String -ArgumentList (,[System.Security.Cryptography.PemEncoding]::Write('CERTIFICATE',$SignerCertificate.RawData))
+                    }
+
+                    $hash | ConvertTo-Json
+                }
             }
-
-            $result = Set-AuthenticodeSignature -FilePath $fileName -HashAlgorithm $fileDigest -Certificate $codeSignCertificate -TimestampServer $timeServer
-
-            [Microsoft.Extensions.Logging.LoggerExtensions]::LogInformation($Logger,('Set-AuthenticodeSignature {0} {1} {2} {3}' -f $result.SignerCertificate.Thumbprint, $result.Status, $result.StatusMessage, $result.Path), $null )
-
-            $response.ContentType = [Net.Mime.MediaTypeNames+Application]::Octet
-
-            $contentDisposition = New-Object -TypeName System.Net.Mime.ContentDisposition -ArgumentList 'attachment'
-
-            $contentDisposition.FileName = $_.FileName
-
-            $response.Headers.ContentDisposition = [string]$contentDisposition
-
-            Get-Content -LiteralPath $fileName -AsByteStream -ReadCount 0
         }
         finally
         {
